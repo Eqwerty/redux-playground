2b681583b267ea57fe25f823ded0ad8c3f718b52
Eduardo Quintana <edquintest@gmail.com>
2025-06-02 22:57:15 +0200

    Add flag state in counter

--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -1,14 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../store";
 import { increment, selectCount } from "./counterSlice";
 
 export function Counter() {
   const count = useSelector(selectCount);
+  const flag = useSelector((state: RootState) => state.flag.value);
   const dispatch = useDispatch();
 
   return (
     <div>
       <button onClick={() => dispatch(increment())}>
-        Increment (<span>{count}</span>)
+        Increment (
+        <span>
+          {count} - {flag.toString()}
+        </span>
+        )
       </button>
     </div>
   );
