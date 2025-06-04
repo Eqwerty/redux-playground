af906a1c52285e9351f1c3a8d317a156bc3c7e37
Eduardo Quintana <edquintest@gmail.com>
2025-06-04 18:14:15 +0200

    Rename FlagToggler to flag

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Counter } from "./counter/Counter";
-import { FlagToggler } from "./flag/FlagToggler";
+import { Flag } from "./flag/Flag";
 
 export default function App() {
   return (
     <div className="App">
       <Counter />
       <br />
-      <FlagToggler />
+      <Flag />
     </div>
   );
 }
--- src/flag/Flag.tsx
+++ src/flag/Flag.tsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { toggle } from "./flagSlice";
 
-export function FlagToggler() {
+export function Flag() {
   const dispatch = useDispatch();
 
   return <button onClick={() => dispatch(toggle())}>Toggle Flag</button>;
